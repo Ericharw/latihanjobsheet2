5696b63329aaee5d368384a8c877ff6bad21d521
Ericharw <ericharizki12@gmail.com>
2023-09-11 09:29:44 +0700

    latihanjobsheet2

---
+++ Latihan09.java
@@ -0,0 +1,7 @@
+public class Latihan09 {
+    public static void main (String[] args) {
+        System.out.print("Nama saya Ericha Rizki Wardani");
+       // System.out.print("\nsaya adalah mahasiswa baru Jurusan Teknologi Informasi-POLINEMA");
+       
+    }
+}
